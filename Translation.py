2d1fd876ffc429008b00c80663e65a291ad025d4
azurezhc <azurezhc@gmail.com>
2023-08-18 19:01:12 +0800

    # 修改一下Readme 以及python脚本

--- Translation.py
+++ Translation.py
@@ -19,24 +19,29 @@ def ReplaceImgPath(content):
     image_path = image_path.strip().strip('"')
     if image_path.startswith('../'):
         return content.group()
-    
+
     return '<img src="../' + image_path + '">'
 
 
-for dirpath, dirname, filenames in os.walk(rootdir):
-    for filepath in filenames:
-        # 检测md结尾文档
-        if filepath.endswith('.md'):
-            # 拼接文件路径
-            file = os.path.join(dirpath, filepath)
-            print(file)
-            # 打开文件
-            f = open(file, 'r+', encoding='utf-8')
-            # 读取文件内容
-            content = f.read()
-            # 替换文档中的图片路径
-            after_content = re.sub(pattern, ReplaceImgPath, content)
-            # content 写入文件
-            f.seek(0)
-            f.write(after_content)
-            f.close()
+def ScanContentFile():
+    for dirpath, dirname, filenames in os.walk(rootdir):
+        for filepath in filenames:
+            # 检测md结尾文档
+            if filepath.endswith('.md'):
+                # 拼接文件路径
+                file = os.path.join(dirpath, filepath)
+                print(file)
+                # 打开文件
+                f = open(file, 'r+', encoding='utf-8')
+                # 读取文件内容
+                content = f.read()
+                # 替换文档中的图片路径
+                after_content = re.sub(pattern, ReplaceImgPath, content)
+                # content 写入文件
+                f.seek(0)
+                f.write(after_content)
+                f.close()
+
+
+if __name__ == "__main__":
+    ScanContentFile()
